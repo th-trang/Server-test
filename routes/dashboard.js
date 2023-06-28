537c28e6966ddf62ed78c6c192e5e7101211f1c4
tnguyen681 <core.editor>
2023-06-28 09:58:04 +0700

    middleware + add data to db

---
+++ routes/dashboard.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { body } = require('express-validator');
+const router = express.Router();
+const dashboardController = require('../controllers/dashboard');
+const auth = require('../middleware/auth')
+
+router.get('/', auth, dashboardController.fetchAll);
+
+router.post(
+  '/',
+  [
+    auth,
+    body('tag').trim().not().isEmpty(),
+    body('name').trim().isLength({ max: 15 }),
+    body('expectedValue'),
+    body('realtimeValue'),
+    body('unit').trim(),
+    body('designP').trim(),
+    body('upperbound').trim(),
+    body('lowerbound').trim()
+  ],
+
+  dashboardController.postData,
+);
+
+// router.put(":/tag", dashboardController.update)
+
+module.exports = router;
